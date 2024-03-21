1af79d3b024b63f590e846314342f2a7dbdfb6cd
LourencoGarces <lou5.braga@gmail.com>
2024-03-21 23:24:20 +0000

    Melhoria nos comentários, e adição de modal(por editar)

---
+++ Scripts/Index.js
@@ -0,0 +1,7 @@
+// Reference to the modal and the close button
+var modal = document.getElementById("myModal"); // Get the modal by its ID
+
+// Add a click event to the profile button to open the modal
+document.getElementById("profileButton").addEventListener("click", function() {
+    modal.style.display = "block"; // Set the modal style to 'block' to show it
+});
