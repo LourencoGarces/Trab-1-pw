4946ed6818964f5461a3ea3f506f5dab66b67677
LourencoGarces <lou5.braga@gmail.com>
2024-03-22 22:13:07 +0000

    Adição de um inicio de modal e atividade em js dos boões log in e register

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -5,3 +5,19 @@ var modal = document.getElementById("myModal"); // Get the modal by its ID
 document.getElementById("profileButton").addEventListener("click", function() {
     modal.style.display = "block"; // Set the modal style to 'block' to show it
 });
+
+// Function to redirect to the login page
+function redirectToLogin() {
+    window.location.href = "Login.html"; // Replace "Login.html" with the URL of your login page
+}
+
+// Function to redirect to the register page
+function redirectToRegister() {
+    window.location.href = "Register.html"; // Replace "Register.html" with the URL of your register page
+}
+
+// Adds an event listener for the Log In button
+document.querySelector('.LogIn').addEventListener('click', redirectToLogin);
+
+// Adds an event listener for the Register button
+document.querySelector('.Register').addEventListener('click', redirectToRegister);
