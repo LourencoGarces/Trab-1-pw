dab19705751bb9460e7475ea49ced43be737564a
LourencoGarces <lou5.braga@gmail.com>
2024-03-25 13:13:20 +0000

    Adição das atividades do modal

--- Scripts/autentication.js
+++ Scripts/autentication.js
@@ -22,6 +22,27 @@ document.body.addEventListener('click', function(event) {
     }
 });
 
+// Reference to the modal and the close button
+var modal = document.getElementById("myModal"); // Get the modal by its ID
+
+// Add a click event to the profile button to open the modal
+document.getElementById("profileButton").addEventListener("click", function() {
+    modal.style.display = "block"; // Set the modal style to 'block' to show it
+});
+
+// Function to redirect to the login page
+function redirectToLogin() {
+    window.location.href = "Login.html"; // Replace "Login.html" with the URL of your login page
+}
+
+// Function to redirect to the register page
+function redirectToRegister() {
+    window.location.href = "Register.html"; // Replace "Register.html" with the URL of your register page
+}
+
+// Adds an event listener for the Log In button
+document.querySelector('.LogIn').addEventListener('click', redirectToLogin);
+
 // autenticacao.js for Login.html
 
 function fazerLogin() {
