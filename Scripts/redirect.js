481d464c397cfad57c585090ff2b64aeffdc70a5
eduardo.j <eduardo.j@ipvc.pt>
2024-03-26 19:35:42 +0000

    alterações eduard_finais

---
+++ Scripts/redirect.js
@@ -0,0 +1,13 @@
+// Function to redirect to the register page
+function redirectToRegister() {
+    window.location.href = "Register.html"; // Replace "Register.html" with the URL of your register page
+}
+// Function to redirect to the index page
+function redirectToIndex() {
+    window.location.href = "Index.html"; // Replace "Index.html" with the URL of your index page
+}
+
+// Function to redirect to the login page
+function redirectToLogin() {
+    window.location.href = "Login.html"; // Replace "Login.html" with the URL of your login page
+}
