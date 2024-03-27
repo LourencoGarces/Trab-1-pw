266ee067049a4c27ac236f5a8c2a09aadd123aa8
LourencoGarces <lou5.braga@gmail.com>
2024-03-27 21:26:44 +0000

    Correção de scripts não necessários

--- Scripts/login.js
+++
@@ -1,8 +0,0 @@
-// Function to redirect to the register page
-function redirectToRegister() {
-    window.location.href = "Register.html"; // Replace "Register.html" with the URL of your register page
-}
-// Function to redirect to the index page
-function redirectToIndex() {
-    window.location.href = "Index.html"; // Replace "Index.html" with the URL of your index page
-}
