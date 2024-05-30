faf11fd85bca7c6ac57a38b4fa9f5281e2dabf0c
LourencoGarces <lou5.braga@gmail.com>
2024-05-30 19:25:27 +0100

    Endpoints da tabela Prices

--- Routes/Pgs/Index.js
+++ Routes/Pgs/Index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const productsRouter = require('./Products');
 const usersRouter = require('./Users');
 const categoriesRouter = require('./Categories');
-const assessRouter = require('./Assess')
+const assessRouter = require('./Assess');
+const pricesRouter = require('./Prices');
 
 router.use('/Products', productsRouter);
 router.use('/Users', usersRouter);
 router.use('/Categories', categoriesRouter);
 router.use('/Assess', assessRouter);
+router.use('/Prices', pricesRouter);
 
 module.exports = router;
